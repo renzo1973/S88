403320b0f9f7fe84bc77ed7a12a5aef87588eae2
renzo1973 <34170618+renzo1973@users.noreply.github.com>
2026-07-05 19:46:12 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-// Configurazione centralizzata di Supabase
-const SUPABASE_URL = "https://iaksgzrbewmxmuyuxgaa.supabase.co"; 
-const SUPABASE_ANON_KEY = "INCOLLA_QUI_LA_CHIAVE_ANON_CORRETTA";
-
-// Esportiamo l'istanza del client per poterla usare negli altri file .js
-export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Array di memoria per le estrazioni (condiviso tra i moduli)
-export let databaseEstrazioni = { data: [] };
